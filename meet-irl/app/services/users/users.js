aca736cadddec67f30f3ba2eac130e0a72e50ebb
Jefferson Lim <jefferer@gmail.com>
2016-09-12 17:32:03 -0700

    [feature] set up karma jasmine test files and initiated tests

---
+++ meet-irl/app/services/users/users.js
@@ -0,0 +1,19 @@
+(function() {
+	'use strict';
+
+	angular.module('api.users', [])
+
+	.factory('Users', function() {
+		var Users = {};
+
+		Users.all = function() {
+			
+		}
+
+
+		// Users.method = function() {};
+
+		return Users;
+	});
+
+})();
---
+++ meet-irl/app/services/users/users.spec.js
@@ -0,0 +1,23 @@
+
+describe('Users factory', function() {
+  var Users;
+
+  // Before each test load our api.users module
+  beforeEach(angular.mock.module('api.users'));
+
+  // Before each test set our injected Users factory (_Users_) to our local Users variable
+  beforeEach(inject(function(_Users_) {
+    Users = _Users_;
+  }));
+
+  // A simple test to verify the Users factory exists
+  it('should exist', function() {
+    expect(Users).toBeDefined();
+  });
+
+  describe('all()', function() {
+  	it('should exist', function() {
+  		expect(Users.all).toBeDefined();
+  	});
+  });
+});
